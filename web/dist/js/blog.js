f480acccbca53fa4b55dad6bb74734ebdf0e1295
may <mayxiaoyu1223@gmail.com>
2014-03-19 23:48:06 +0800

    前端 初始化

--- web/dist/js/blog.js
+++ web/dist/js/blog.js
@@ -1,3 +1,10 @@
 /**
  * Created by may on 14-3-13.
  */
+/**
+ * 标题的背景色
+ */
+$(".blog-nav-item").click(function(){
+    $(".navbar-nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+});
